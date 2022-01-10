75e48f2151781aaeea1587884027d06972d7ea60
JainAyush09 <jainau@rknec.edu>
2022-01-09 19:00:00 -1100

    QuickStory

---
+++ app/src/main/java/com/example/planeto/LoginQuickStory.kt
@@ -0,0 +1,11 @@
+package com.example.planeto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginQuickStory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_quick_story)
+    }
+}
---
+++ app/src/main/java/com/example/planeto/setData.kt
@@ -0,0 +1,108 @@
+package com.example.planeto
+
+object setData {
+    fun SetPlanets():List<PlanetData>{
+        var Planetlist= mutableListOf<PlanetData>()
+        Planetlist.add(
+            PlanetData(
+                1,
+                "one",
+                "Chapter 1",
+                "1",
+                "1",
+                "Jana gana mana … The schoolboy squirmed. Another two minutes? He knew he should stand at attention. The drillmaster's cane loomed large.\"Vindhya Himachal …\"He grunted in discomfort. This was unbearable. He considered making a dash; after all he was in the last row. What if the master noticed? The cane loomed again. He gritted his teeth.\"Tava shubha …\"This is it. He cast his eyes around.\"Jaya he …\"He started running.\"Jaya he …\"He was almost there.\"Jaya he …\"The chorus floated from afar. He was already in the toilet, heaving a relieved sigh."
+            )
+        )
+        Planetlist.add(
+            PlanetData(
+                2,
+                "two",
+                "Chapter 1",
+                "1",
+                "1",
+                "On a windy winter morning, a woman looked out of the window.The only thing she saw, a garden. A smile spread across her face as she spotted Maria, her daughter, in the middle of the garden enjoying the weather. It started drizzling. Maria started dancing joyfully.She tried to wave to her daughter, but her elbow was stuck, her arm hurt, her smile turned upside down. Reality came crashing down as the drizzle turned into a storm. Maria's murdered corpse consumed her mind.On a windy winter morning, a woman looked out of the window of her jail cell."
+            )
+        )
+        Planetlist.add(
+            PlanetData(
+                3,
+                "three",
+                "Chapter 1",
+                "1",
+                "1",
+                "The country was on fire. Communal riots had paralyzed most of the state. Reyaz, with the help of a friend, got a fake identity card--his new name was Rakesh--and booked a ticket to Aligarh. The ticket checker on the train asked for his identification--Reyaz nervously showed the one he had recently procured. He seemed satisfied and Reyaz heaved a sigh of relief.At Aligarh there was none to fear. \"Assalamu alaikum,\" said Reyaz to ward off a group of enraged people. The angriest of them, with bloodshot eyes, approached Reyaz and asked for his identity card."
+            )
+        )
+        Planetlist.add(
+            PlanetData(
+                4,
+                "four",
+                "Chapter 1",
+                "1",
+                "1",
+                "She peered over the open magazine, and there he was, still staring at her, disconcertingly. For the past 30 minutes, she'd endured his irritating attention. Time to call airport security. The burly cop strode in purposefully, with a sleek Alsatian on leash. \"Sir, there's been a complaint. I need you to come with me. Quietly, please,\" he growled. The leather-jacketed man didn't move a muscle. His hands were rock-steady on the trolley handle in front of him. The cop waited for a minute, and then reached out to handcuff the Ray-Ban-wearing guy. The hands were locked in rigor mortis."
+            )
+        )
+        Planetlist.add(
+            PlanetData(
+                5,
+                "five",
+                "Chapter 1",
+                "1",
+                "1",
+                "Do you believe in shooting stars?\" she asked.\"Do you?\"\"There is no harm, is there?\" She paused. \"I'd love to sit in the balcony amidst all the flowerpots and watch the busy world go by.\"He said nothing. She needed no assurance, no promise. She squawked a reply when they asked if she was ready to go back to her room. It would be another 10 minutes before the duty nurse wheeled him away.She had laughed at the last tooth he had lost. He had teased her about the silver hair at the back of her sweater."
+            )
+        )
+        Planetlist.add(
+            PlanetData(
+                6,
+                "six",
+                "Chapter 1",
+                "1",
+                "1",
+                "Hearing a knock on the door,  she hustled towards it with her  little feet, her lips uncloaking the cutest smile and her voice singing, \"Daddy's home!\" Her mum, glued to the news channels for the past week, approached the door hesitantly and opened it with trepidation.Two men in military uniform were standing at the doorstep. One of them handed her an envelope with a mournful expression, adding plaintively, \"We're sorry, Mrs Bhatt.\"\"Where's my dad, Uncle? He promised we'll celebrate Diwali together this time,\" exclaimed the girl. They stared helplessly, with a lump in their throats and moistened eyes."
+            )
+        )
+        Planetlist.add(
+            PlanetData(
+                7,
+                "seven",
+                "Chapter 1",
+                "1",
+                "1",
+                "They met at a cafe, stealing glances at each other while the parents  spoke animatedly.They remained silent throughout, only exchanging shy smiles while ordering snacks at the counter.Returning with the food, he moved to the head of the table to get a good look at her.Noticing his manoeuvre, she smiled down at her coffee, making him beam like a proud schoolboy.When the two families parted at the end of the meeting, he rushed back to the cafe, praying that the girl, who had been at the table behind theirs all afternoon, would still be there."
+            )
+        )
+        Planetlist.add(
+            PlanetData(
+                8,
+                "eight",
+                "Chapter 1",
+                "1",
+                "1",
+                "It was pouring, as I entered a  nearby porch.Out of the blue, a kid startled me from behind--I panicked and scampered away. His father asked him not to scare anyone.After some initial hiccups we became good friends. I often visited their house, ate with them.One day, while I was slurping down the milk, a man entered their portico, begging for food.The father yelled at him and pushed him out of the entrance.I was terrified, and in a jiffy, I ran away screaming, \"Meow! Meow …\""
+            )
+        )
+        Planetlist.add(
+            PlanetData(
+                9,
+                "nine",
+                "Chapter 1",
+                "1",
+                "1",
+                "Border guard Melissa Walter fumed, \"Madam President's lost it.\" A new batch was arriving. The count had crossed 10,000. \"As if the country doesn't have enough mouths to feed.\"Officer Gerald was off-duty, so here she was, about to 'welcome' refugees. The boat arrived. She pasted on her best professional smile.So many people, all skinny and gaunt. Teary, scared eyes, with a weak gait. Clinging to the elders, the children walked on.\"Look!\" a boy exclaimed, dropping down. \"The sand is so soft here. It's not red. Can I touch, Mama?\" he pleaded.Melissa stood still, stunned into silence."
+            )
+        )
+        Planetlist.add(
+            PlanetData(
+                10,
+                "ten",
+                "Chapter 1",
+                "1",
+                "1",
+                "Varun called his friend over to his house. When he arrived, he told him he had to speak to him about a problem. They both went up to Varun's room.\"What is it?\" asked the friend. \"I think I am having an identity crisis,\" said Varun.\"What do you mean?\" asked his friend.\"MOOOOOO!\" he bellowed like a cow.His friend stood frozen, in stunned silence. Varun burst out laughing, \"I was just kidding!\"\"Are you sure? Because we just ran out of milk,\" came the reply."
+            )
+        )
+        return Planetlist
+    }
+}
